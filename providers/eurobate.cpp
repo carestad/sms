33d24f93d964525ddea44b66b36e479dae0e68a3
Espen Volden <voldern@harry.(none)>
2009-03-19 22:55:27 +0100

    There is no need to login @ eurobate

--- providers/eurobate.cpp
+++ providers/eurobate.cpp
@@ -1,24 +1,12 @@
 #include "eurobate.h"
 #include <string>
+#include <sstream>
 #include <iostream>
 #include "curl/curl.h"
 
 void Eurobate::setup(std::string username, std::string password)
 {
-  CURL *curl;
-  curl = curl_easy_init();
-
-  std::string post = "username=%s&passwd=%s&op=2login&lang=norwegian";
-  char data[post.length() + username.length() + password.length() + 1];
-  sprintf(data, post.c_str(), username.c_str(), password.c_str());
-
-  std::cout << "POST: " << data << std::endl;
-  
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
-  curl_easy_setopt(curl, CURLOPT_URL, 
-                   "http://eurobate.com/index.php?option=com_profilemgr&task=login");
-  curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "cookies.txt");
-  curl_easy_perform(curl);
+  // No need to login at eurobate
 }
 
 bool Eurobate::send(std::string to, std::string from, std::string message)
