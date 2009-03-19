874acdf8c36125875910dd6c5312630761154795
Espen Volden <voldern@harry.(none)>
2009-03-19 20:27:09 +0100

    Initial import of provider eurobate

---
+++ providers/eurobate.cpp
@@ -0,0 +1,13 @@
+#include "eurobate.h"
+#include <string>
+
+void Eurobate::setup(std::string username, std::string password)
+{
+
+}
+
+bool Eurobate::send(int to, std::string message)
+{
+
+  return true;
+}
---
+++ providers/eurobate.h
@@ -0,0 +1,14 @@
+#ifndef __EUROBATE_H__
+#define __EUROBATE_H__
+
+#include "provider.h"
+#include <string>
+
+class Eurobate : public Provider
+{
+ public:
+  void setup(std::string username, std::string password);
+  bool send(int to, std::string message);
+};
+
+#endif //__EUROBATE_H__
